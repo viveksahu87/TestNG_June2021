e357466533e5f6a9aa13fee573344a1e0abe2723
viveksahu87 <82751754+viveksahu87@users.noreply.github.com>
2021-06-28 16:49:11 +0530

    update
    update

--- src/test/java/VivekTechnologies/base/TestBase.java
+++ src/test/java/VivekTechnologies/base/TestBase.java
@@ -1,11 +1,21 @@
 package VivekTechnologies.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
     public static WebDriver driver;
+    public static Properties config = new Properties();
+    public static Properties OR = new Properties();
+    public static FileInputStream fis;
+
     /*
     *initialising
     *WebDriver
@@ -17,8 +27,30 @@ public class TestBase {
     * Mail
     * */
 
+
     @BeforeSuite
-    public void setup(){
+    public void setup() throws FileNotFoundException {
+
+        if(driver==null){
+
+            FileInputStream fis= new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\Config.properties");
+            try {
+                config.load(fis);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\OR.properties");
+            try {
+                OR.load(fis);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(config.getProperty("browser").equals("chrome")){
+                driver =new ChromeDriver();
+            }
+
+        }
 
     }
     @AfterSuite
--- src/test/java/VivekTechnologies/rough/TestProperties.java
+++ src/test/java/VivekTechnologies/rough/TestProperties.java
@@ -10,9 +10,14 @@ public class TestProperties {
 
     public static void main(String[] args) throws IOException {
         Properties config = new Properties();
+        Properties OR = new Properties();
         FileInputStream fis= new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\Config.properties");
         config.load(fis);
+        fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\OR.properties");
+        OR.load(fis);
+
         System.out.println(config.getProperty("browser"));
         System.out.println(config.getProperty("testsiteurl"));
+        System.out.println(OR.getProperty("signin"));
     }
 }

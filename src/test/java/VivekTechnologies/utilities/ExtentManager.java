941d7b7457b661db0b8501b76b81ef65cb095f4e
viveksahu87 <82751754+viveksahu87@users.noreply.github.com>
2021-07-15 12:52:54 +0530

    16
    16

--- src/test/java/VivekTechnologies/utilities/ExtentManager.java
+++ src/test/java/VivekTechnologies/utilities/ExtentManager.java
@@ -7,11 +7,9 @@ public class ExtentManager {
 
     private static ExtentReports extent;
 
-
     public static ExtentReports getInstance(){
 
         if(extent==null){
-
 
             extent = new ExtentReports(System.getProperty("user.dir")+"\\target\\surefire-reports\\html\\extent.html",true,DisplayOrder.OLDEST_FIRST);
             extent.loadConfig(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));
